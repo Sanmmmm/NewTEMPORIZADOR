a490595cd41ec808d8a2f57cea948315235fb3f8
Sanmmmm <samngof@gmail.com>
2024-08-28 23:50:50 -0300

    Add files via upload

---
+++ TEMPORIZADOR/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace TEMPORIZADOR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CRONOMETRO = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CRONOMETRO
+            // 
+            this.CRONOMETRO.AutoSize = true;
+            this.CRONOMETRO.Font = new System.Drawing.Font("Arial Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CRONOMETRO.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.CRONOMETRO.Location = new System.Drawing.Point(61, -7);
+            this.CRONOMETRO.Name = "CRONOMETRO";
+            this.CRONOMETRO.Size = new System.Drawing.Size(519, 85);
+            this.CRONOMETRO.TabIndex = 0;
+            this.CRONOMETRO.Text = "CRONOMETRO";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(505, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(533, 135);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "00:00:00";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(815, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 30);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(607, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "INICIAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(324, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 35);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "PARAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(56, 376);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "FECHAR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(63, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(245, 193);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(425, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(577, 85);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "TEMPORIZADOR";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button6.Location = new System.Drawing.Point(324, 293);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(114, 35);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "INICIAR";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(136, 297);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 35);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "FECHAR";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Location = new System.Drawing.Point(324, 179);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(114, 30);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "PAUSE";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(324, 232);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 38);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "REDEFINIR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 357);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CRONOMETRO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CRONOMETRO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TEMPORIZADOR/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEMPORIZADOR
+{
+    public partial class Form1 : Form
+    {
+        int tempo = 0;
+        int minuto = 0;
+        int segundo = 0;
+        int horas = 0;
+        bool isPaused = false;
+        int zerar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length >0)
+            {
+                pictureBox1.Visible = false;
+                tempo = Convert.ToInt16(textBox1.Text);
+                if (tempo >= 60)
+                {
+
+                    minuto = tempo / 60;
+                    segundo = tempo % 60;
+                }
+                else
+                {
+                    minuto = 0;
+                    segundo = tempo;
+                }
+                label1.Text = minuto.ToString().PadLeft(2, '0') + ":" + segundo.ToString().PadLeft(2, '0');
+                timer1.Enabled = true;
+                button7.Text = "PAUSE";
+
+
+            }
+            else
+            {
+                MessageBox.Show("Entre com um digito" );
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            timer1.Enabled = false;
+            timer2.Enabled = false;// Para o timer
+            isPaused = false; // Garante que o estado de pausa é redefinido
+            label1.Text = "00:00:00"; // Reseta o display para zero
+
+            // Redefine todas as variáveis de tempo
+            segundo = 0;
+            minuto = 0;
+            horas = 0;
+
+            // Limpa o conteúdo da caixa de texto (se for um cronômetro de contagem regressiva)
+            textBox1.Text = "";
+
+            // Opcional: Atualiza o texto do botão de pausar/retomar
+            button7.Text = "PAUSE"; // Caso o cronômetro/temporizador esteja pausado
+        }
+
+      
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                segundo--;
+                if (minuto > 0)
+                {
+                    if (segundo < 0)
+                    {
+                        segundo = 59;
+                        minuto--;
+                    }
+                }
+                label1.Text = horas.ToString().PadLeft(2, '0') + ":" + minuto.ToString().PadLeft(2, '0') + ":" + segundo.ToString().PadLeft(2, '0');
+                if (minuto == 0 && segundo == 0)
+                {
+                    timer1.Enabled = false;
+                    pictureBox1.Visible = true;
+
+                }
+            
+          
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.Visible = false;
+            timer2.Enabled = true;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            segundo++;
+            if (segundo == 60)
+            {
+                minuto++;
+                segundo = 0;
+            }
+            if (minuto == 60)
+            {
+                horas++;
+                minuto = 0;
+            }
+            label1.Text = horas.ToString().PadLeft(2, '0') + ":" + minuto.ToString().PadLeft(2, '0') + ":" + segundo.ToString().PadLeft(2, '0');
+            if (minuto == 0 && segundo == 0)
+            {
+                timer2.Enabled = false;
+                pictureBox1.Visible = true;
+
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                isPaused = !isPaused; // Toggle pause state
+                if (isPaused)
+                {
+                    timer2.Enabled = false;// Stop the timer
+                    button7.Text = "RETOMAR"; // Change button text to "Resume"
+                }
+                else
+                {
+
+                    timer2.Enabled = true;// Resume the timer
+                    button7.Text = "PAUSE"; // Change button text to "Pause"
+                }
+            }
+            else
+            {
+                isPaused = !isPaused; // Toggle pause state
+                if (isPaused)
+                {
+                    timer1.Enabled = false;
+                    // Stop the timer
+                    button7.Text = "RETOMAR"; // Change button text to "Resume"
+                }
+                else
+                {
+                    timer1.Enabled = true;
+                    // Resume the timer
+                    button7.Text = "PAUSE"; // Change button text to "Pause"
+                }
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        { if (textBox1.Text.Length > 0)
+            {
+               
+                zerar = Convert.ToInt16(textBox1.Text);
+                if (zerar >= 60)
+                {
+
+                    minuto = zerar / 60;
+                    segundo = zerar % 60;
+                }
+                else
+                {
+                    minuto = 0;
+                    segundo = zerar;
+                }
+                label1.Text = minuto.ToString().PadLeft(2, '0') + ":" + segundo.ToString().PadLeft(2, '0');
+                timer1.Enabled = false;
+
+                textBox1.Text = Convert.ToString(zerar);
+                button7.Text = "RETOMAR";
+            }
+            else
+            {
+        
+                timer2.Enabled = false;
+                label1.Text = "00:00:00";
+                segundo = 0;
+                minuto = 0;
+                horas = 0;
+                button7.Text = "PAUSE";
+            }
+        }
+    }
+
+ }
+
+
